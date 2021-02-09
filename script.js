2257520f9106d5e2dcc1372cf4a71d724db955ff
stickman-4413 <78157722+stickman-4413@users.noreply.github.com>
2021-02-09 22:52:33 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,81 @@
+$(function () {
+
+
+  //ページ内スクロール
+  $('a[href^="#"]').on('click', function () {
+    var speed = 300;
+    var href = $(this).attr("href");
+    var target = $(href == "#" || href == "" ? 'html' : href);
+    var position = target.offset().top;
+    $("html, body").animate({
+      scrollTop: position
+    }, speed, "swing");
+    return false;
+  });
+
+  //ページトップへ戻る
+  var $pageTop = $('.page-top');
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 300) {
+      $pageTop.fadeIn();
+    } else {
+      $pageTop.fadeOut();
+    }
+  });
+  $pageTop.on('click', function () {
+    $('body,html').animate({
+      scrollTop: 0
+    }, 300);
+    return false;
+  });
+
+  //スクロールに応じてヘッダーの背景色が変化
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 0) {
+      $('.header').addClass('active');
+    } else {
+      $('.header').removeClass('active');
+    }
+  });
+});
+
+//Swiperの設定
+var mySwiper = new Swiper('.swiper-container', {
+  // Optional parameters
+  autoplay: {
+    delay: 4000,
+  },
+  speed: 500,
+  loop: true,
+  effect: 'fade',
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+})
+
+$(function(){
+	$(window).scroll(function (){
+		$('.fadein').each(function(){
+			var elemPos = $(this).offset().top;
+			var scroll = $(window).scrollTop();
+			var windowHeight = $(window).height();
+			if (scroll > elemPos - windowHeight +100 ){
+				$(this).addClass('scrollin');
+			}
+		});
+	});
+});
